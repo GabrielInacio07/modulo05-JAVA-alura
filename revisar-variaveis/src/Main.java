be25b591e761e7b86efb30ce1fcd178568caf469
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-01 13:42:46 -0300

    Segundo Desafio do módulo de revisão (FAZENDO CASTING DE VALROES)

--- revisar-variaveis/src/Main.java
+++ revisar-variaveis/src/Main.java
@@ -1,19 +1,20 @@
 import D01.PessoaEstudante;
 
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-    System.out.println("Revisão de TIPOS/VARIÁVEIS");
+        System.out.println("Revisão de TIPOS/VARIÁVEIS");
+        System.out.println("Exercício fazendo CASTING DE VALORES");
+        Scanner input = new Scanner(System.in);
+        double estoque = 0;
 
-        PessoaEstudante pessoa_estudante = new PessoaEstudante("João",21,1.90,false);
+        System.out.println("Digite a quantidade de estoque a inserir");
+        estoque = Double.parseDouble(input.nextLine());
 
-        //Verificando escopo
+        System.out.println("Valor passado pelo usuário: " + estoque);
+        System.out.println("Valor incrementado no estoque em inteiro: " + (int) estoque);
 
-        if (pessoa_estudante.isEstudante()){
-            System.out.println(pessoa_estudante.getNome() + " é um estudante");
-            System.out.println(pessoa_estudante.toString());
-        }else{
-            System.out.println(pessoa_estudante.getNome() + " não é um estudante");
-        }
 
     }
 }
