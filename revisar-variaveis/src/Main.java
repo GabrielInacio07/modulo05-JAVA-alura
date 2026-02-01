a34f28c7e0de7063a56bed423e10201e311dbe74
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-02-01 13:12:16 -0300

    Fazendo CASTING de tipos em variáveis

--- revisar-variaveis/src/Main.java
+++ revisar-variaveis/src/Main.java
@@ -2,5 +2,11 @@ class Main{
     public static void main(String[] args) {
     System.out.println("Revisão de TIPOS/VARIÁVEIS");
 
+        int quantidadeDePassos = 500;
+        double alturaUser = 1.85;
+        String nome = "Gabriel";
+
+        int alturaEmCentimetro = (int) (alturaUser * 100);
+        System.out.println(alturaEmCentimetro);
     }
 }
